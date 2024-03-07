92fba2c2d6d3ea1200d2d23edd0763462e67bb0c
Henrique Mancini Nevado <henriquemancininevado@gmail.com>
2024-03-07 10:05:28 -0300

    Variables, console.log and first assignment

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,28 @@
+/* 
+    * To declare a variable in JavaScript we use either the keyword "var", or the keyword "let"
+    * The usual convention for variable names is camelCase (first letter lowercase and all the subsequent uppercase)
+    ! Variable names CANNOT start with number
+    ! Variable names can only contain letters, numbers, underscores or dollar signs
+    ! Variable names CANNOT be reserved keywords (avoid "name" as well)
+    ! Variable names should not be started with an uppercase letter
+    * Constants (variables that won't have its value changed) are named with an uppercase
+    * Variable names should be descriptive, that is, you should know right away what value are they storing
+*/
+
+let firstName = "Henrique";
+
+// The function console.log() is used to write something in the console
+console.log(firstName);
+
+/* 
+    * Assignment 1:
+    1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+    2. Log their values to the console 
+
+
+let country = "Brazil";
+let continent = "South America";
+let populationInMillions = "214.3";
+
+console.log(country, "-", continent, populationInMillions); // + console.log can receive more than one value at a time and also concatenate strings
+*/

9ccaa0a5dc88ba23fce80a9e55e158b3deda9b6c
Henrique Mancini Nevado <henriquemancininevado@gmail.com>
2024-04-08 09:49:27 -0300

    Coding Challenge 01 - Calculating BMI

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -56,3 +56,69 @@ console.log(
         language +
         "."
 );
+
+/* Challenge 01
+ Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+
+ Your task is to write some code to help them:
+
+    1. Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+
+    2. Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+
+    3. Log the value of BMIMark and BMIJohn to the console.
+
+    BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+ TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+ TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+*/
+
+/*
+let massMark, heightMark, massJohn, heightJohn;
+let BMIMark, BMIJohn;
+let markHigherBMI;
+*/
+
+/*
+ TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+
+massMark = 78;
+heightMark = 1.69;
+BMIMark = massMark / (heightMark * heightMark);
+
+massJohn = 92;
+heightJohn = 1.95;
+BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if(BMIMark > BMIJohn){
+    markHigherBMI = true;
+}else{
+    markHigherBMI = false;
+}
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
+
+/*
+ TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+
+massMark = 95;
+heightMark = 1.88;
+BMIMark = massMark / (heightMark * heightMark);
+
+massJohn = 85;
+heightJohn = 1.76;
+BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if(BMIMark > BMIJohn){
+    markHigherBMI = true;
+}else{
+    markHigherBMI = false;
+}
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
